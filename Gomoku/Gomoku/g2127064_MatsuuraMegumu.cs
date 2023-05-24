77282be90ff849e1ec8685a4aec899409cf0ad0d
megutan30 <98795663+megutan0330@users.noreply.github.com>
2023-05-24 16:22:24 +0900

    スクリプト修正

--- Gomoku/Gomoku/g2127064_MatsuuraMegumu.cs
+++ Gomoku/Gomoku/g2127064_MatsuuraMegumu.cs
@@ -8,8 +8,8 @@ namespace Sample
 {
     public class Gomoku
     {
-        private const int BOARD_SIZEX = 10; // 盤面のサイズ
-        private const int BOARD_SIZEY = 12; // 盤面のサイズ
+        private const int BOARD_SIZEX = 12; // 盤面のサイズ
+        private const int BOARD_SIZEY = 8; // 盤面のサイズ
         private const string endKeyWord = "end"; // 盤面のサイズ
 
         public enum Stone
@@ -22,10 +22,15 @@ namespace Sample
 
         private static readonly Stone[,] board = new Stone[BOARD_SIZEX, BOARD_SIZEY]; // 盤面を表す二次元配列
 
+        /*コンストラクター*/
+        public Gomoku()
+        {
+            InitBoard();
+        }
+
         /*五目をスタートする関数*/
         public void Start()
         {
-            InitBoard();
             PrintBorad();
             PrintResult();
             CountBoardStone();
@@ -36,11 +41,11 @@ namespace Sample
         {
             //盤面の列の数に応じて番号を表示
             Console.Write("   ");
-            for (int i = 0; i < BOARD_SIZEX; i++)
+            for (int i = 0; i < board.GetLength(0); i++)
             {
                 Console.Write($"{(i + 1),3}");
             }
-            Console.WriteLine();
+            Console.WriteLine("");
 
             //盤面の行の数に応じて盤面を描画
             for (int row = 0; row < BOARD_SIZEY; row++)
@@ -48,7 +53,7 @@ namespace Sample
                 //番号を表示
                 Console.Write($"{row + 1,3}" + " ");
                 //ボード配列の値に応じて碁を配置   0:〇  1:●
-                for (int col = 0; col < BOARD_SIZEX; col++)
+                for (int col = 0; col < board.GetLength(0); col++)
                 {
                     Console.Write(board[col, row] == Stone.BLACK ? "〇 " : "● ");
                 }
@@ -62,9 +67,9 @@ namespace Sample
             Random rand = new();
 
             //ボード配列の値を0or1で初期化
-            for (int row = 0; row < BOARD_SIZEX; row++)
+            for (int row = 0; row < board.GetLength(0); row++)
             {
-                for (int col = 0; col < BOARD_SIZEY; col++)
+                for (int col = 0; col < board.GetLength(1); col++)
                 {
                     board[row, col] = (Stone)rand.Next(2);
                 }
@@ -77,9 +82,9 @@ namespace Sample
             int blackCount = 0;
             int whiteCount = 0;
             //
-            for (int row = 0; row < BOARD_SIZEX; row++)
+            for (int row = 0; row < board.GetLength(0); row++)
             {
-                for (int col = 0; col < BOARD_SIZEY; col++)
+                for (int col = 0; col < board.GetLength(1); col++)
                 {
                     if (board[row, col] == Stone.BLACK)
                     {
@@ -117,8 +122,8 @@ namespace Sample
                 //入力されたデータが変更可能かつ
                 //ボードのないの大きさ内だった時に処理
                 if (split.Length == 2 && int.TryParse(split[0], out int raw) && int.TryParse(split[1], out int con)
-                    && raw >= 1 && raw <= Gomoku.BOARD_SIZEX
-                    && con >= 1 && con <= Gomoku.BOARD_SIZEY)
+                    && raw >= 1 && raw <= Gomoku.board.GetLength(0)
+                    && con >= 1 && con <= Gomoku.board.GetLength(1))
                 {
                     // 指定されたマス目の石の色を表示する
                     Stone stone = Gomoku.board[raw - 1, con - 1];
@@ -131,7 +136,22 @@ namespace Sample
             }
             Console.WriteLine("これで終わりだ！！！");
         }
+
+        /*プレイヤーの挙動*/
+
+        /*コンピュータの挙動*/
+        
+        /*勝敗判定*/
+        
+        /*指定した場所におく*/
+        
+        /**/
+        
+        /**/
+
     }
+
+
     internal class MegutanProject
     {
         static void Main(string[] args)
